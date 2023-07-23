df6143ab2d8146d42eb8da4f0c804c435764ebbc
morgu <morgunov.96@bk.ru>
2023-07-23 12:29:27 +0300

    Creating and adjusting text field and finally adding code commentary

--- src/bank/managment/system/SignUp.java
+++ src/bank/managment/system/SignUp.java
@@ -6,8 +6,11 @@ import java.util.Random;
 
 public class SignUp  extends JFrame {
 
-    Random r1 = new Random();
+    JTextField textName, textFname;
 
+    /* The goal of the 3 below lines is to
+    generate random value for an application form */
+    Random r1 = new Random();
     long first4 = (r1.nextLong()%9000L) + 1000L;
     String first = " " + Math.abs(first4);
 
@@ -25,6 +28,45 @@ public class SignUp  extends JFrame {
         label1.setBounds(160,20,600,40);
         label1.setFont(new Font("AvantGard", Font.BOLD, 30));
         add(label1);
+
+
+        /* textName which function is to take text input from user. As below
+         creating and adjusting  */
+        textName = new JTextField();
+        textName.setFont( new Font("AvantGarde", Font.ITALIC, 14));
+        textName.setBounds(350,200,300,35);
+        add(textName);
+
+        textFname = new JTextField();
+        textFname.setFont( new Font("AvantGarde", Font.ITALIC, 14));
+        textFname.setBounds(350,300,300,35);
+        add(textFname);
+
+
+        // creating and adjusting(the hardest part) text labels
+        JLabel label2 = new JLabel(" Page 1 ");
+        label2.setFont(new Font("AvantGard", Font.BOLD, 30));
+        label2.setBounds(300,80,610,40);
+        add(label2);
+
+        JLabel label3 = new JLabel(" Personal Data ");
+        label3.setFont(new Font("AvantGard", Font.BOLD, 30));
+        label3.setBounds(290,130,600,30);
+        add(label3);
+
+        JLabel labelName = new JLabel(" Name ");
+        labelName.setFont(new Font("AvantGard", Font.BOLD, 30));
+        labelName.setBounds(100,200,100,30);
+        add(labelName);
+
+        JLabel labelFName = new JLabel("Father's Name ");
+        labelFName.setFont(new Font("AvantGard", Font.BOLD, 30));
+        labelFName.setBounds(100,300,250,30);
+        add(labelFName);
+
+
+
+
 
         getContentPane().setBackground(Color.ORANGE);
         setLayout(null);
