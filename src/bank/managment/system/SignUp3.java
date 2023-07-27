5bd4110b9e895f58c1bf0fcb7723a75e0ed06173
morgu <morgunov.96@bk.ru>
2023-07-27 16:47:29 +0300

    SignUp3 spine is finished. Here user can select account type, service type he needed and enter his card numerical info

--- src/bank/managment/system/SignUp3.java
+++ src/bank/managment/system/SignUp3.java
@@ -7,6 +7,7 @@ public class SignUp3 extends JFrame {
 
     JRadioButton r1, r2, r3, r4;
     ButtonGroup br1;
+    JCheckBox b1,b2,b3,b4,b5,b6,b7;
     SignUp3(){
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png")); // to-be-commented
@@ -18,7 +19,7 @@ public class SignUp3 extends JFrame {
 
 
         getContentPane().setBackground(new Color(215,252,252));
-        setSize(850,500);
+        setSize(850,700);
         setLayout(null);
         setLocation(400,20);
         setVisible(true);
@@ -30,13 +31,48 @@ public class SignUp3 extends JFrame {
 
         JLabel l2 = new JLabel("Account Details");
         l2.setFont(new Font("AvantGarde", Font.BOLD, 22));
-        l2.setBounds(280,70,400,40);
+        l2.setBounds(280,70,200,40);
         add(l2);
 
         JLabel l3 = new JLabel("Account Type");
         l3.setFont(new Font("AvantGarde", Font.BOLD, 16));
         l3.setBounds(180,130,400,40);
         add(l3);
+
+        JLabel l4 = new JLabel("Card Number: ");
+        l4.setFont(new Font("AvantGarde", Font.BOLD, 16));
+        l4.setBounds(180,250,200,40);
+        add(l4);
+
+        JLabel l5 = new JLabel("(Your 16-digit card number)");
+        l5.setFont(new Font("AvantGarde", Font.BOLD, 13));
+        l5.setBounds(180,280,250,20);
+        add(l5);
+
+        JLabel l6 = new JLabel("XXXX-XXXX-XXXX-4841");
+        l6.setFont(new Font("AvantGarde", Font.BOLD, 16));
+        l6.setBounds(450,260,250,20);
+        add(l6);
+
+        JLabel l7 = new JLabel("PIN: ");
+        l7.setFont(new Font("AvantGarde", Font.BOLD, 16));
+        l7.setBounds(180,330,250,20);
+        add(l7);
+
+        JLabel l8 = new JLabel("XXXX");
+        l8.setFont(new Font("AvantGarde", Font.BOLD, 16));
+        l8.setBounds(450,330,250,20);
+        add(l8);
+
+        JLabel l9 = new JLabel("(4-digit password)");
+        l9.setFont(new Font("AvantGarde", Font.BOLD, 13));
+        l9.setBounds(180,350,250,20); // ideal placement
+        add(l9);
+
+        JLabel l10 = new JLabel("Services required: ");
+        l10.setFont(new Font("AvantGarde", Font.BOLD, 16));
+        l10.setBounds(180,400,250,20); // ideal placement
+        add(l10);
 
         r1 = new JRadioButton("Saving account ");
         r1.setFont(new Font("Avant Garde", Font.BOLD,15));
@@ -61,6 +97,57 @@ public class SignUp3 extends JFrame {
         r4.setBackground(new Color(215,252,252));
         r4.setBounds(170,197,250,30);
         add(r4);
+
+        br1 = new ButtonGroup();
+        br1.add(r1);
+        br1.add(r2);
+        br1.add(r3);
+        br1.add(r4);
+
+
+        b1 = new JCheckBox("ATM CARD");
+        b1.setBackground(new Color(215,252,252));
+        b1.setFont(new Font("AvantGarde",Font.BOLD,16));
+        b1.setBounds(340,395,200,20);
+        add(b1);
+
+        b2 = new JCheckBox("Internet Banking");
+        b2.setBackground(new Color(215,252,252));
+        b2.setFont(new Font("AvantGarde",Font.BOLD,16));
+        b2.setBounds(540,395,200,20);
+        add(b2);
+
+        b3 = new JCheckBox("Mobile Banking");
+        b3.setBackground(new Color(215,252,252));
+        b3.setFont(new Font("AvantGarde",Font.BOLD,16));
+        b3.setBounds(340,440,200,20);
+        add(b3);
+
+        b4 = new JCheckBox("Email Alert");
+        b4.setBackground(new Color(215,252,252));
+        b4.setFont(new Font("AvantGarde",Font.BOLD,16));
+        b4.setBounds(540,440,200,20);
+        add(b4);
+
+        b5 = new JCheckBox("Mobile Alert");
+        b5.setBackground(new Color(215,252,252));
+        b5.setFont(new Font("AvantGarde",Font.BOLD,16));
+        b5.setBounds(340,490,150,20);
+        add(b5);
+
+        b6 = new JCheckBox("Account check");
+        b6.setBackground(new Color(215,252,252));
+        b6.setFont(new Font("AvantGarde",Font.BOLD,16));
+        b6.setBounds(540,490,200,20);
+        add(b6);
+
+        b7 = new JCheckBox("I declare that the above entered details are correct.");
+        b7.setBackground(new Color(215,252,252));
+        b7.setFont(new Font("AvantGarde",Font.BOLD,11));
+        b7.setBounds(100,580,600,20);
+        add(b7);
+
+
 
 
 
